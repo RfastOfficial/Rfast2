28b493204a87674b4564c95b5f9076cc2a15498b
Michail Tsagris <mtsagris@yahoo.gr>
2019-11-06 21:34:34 +0200

    update gamma.regression.R

--- R/gamma_regression.R
+++ R/gamma_regression.R
@@ -1,11 +1,11 @@
 #[export]
 gammareg <- function(y, x, tol = 1e-07, maxiters = 100) {
   mod <- Rfast::gammacon(y)
-  gamma_reg(Y = y, X = x, mod = mod, tol = tol, maxiters = maxiters) 
+  .Call(Rfast2_gamma_reg, Y = y, X = x, mod = mod, tol = tol, maxiters = maxiters) 
 }
 
 
 #[export]
 gammaregs <- function(y, x, tol = 1e-07, logged = FALSE, parallel = FALSE, maxiters = 100) {
-  gamma_regs(Y = y, X = x, tol = tol, logged = logged, parallel = (parallel > 0), maxiters = maxiters)
+  .Call(Rfast2_gamma_regs, Y = y, X = x, tol = tol, logged = logged, parallel = (parallel > 0), maxiters = maxiters)
 }
