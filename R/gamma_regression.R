62df14c5c978b84b30c2cf0cca14bf307609093a
Michail Tsagris <mtsagris@yahoo.gr>
2019-11-06 21:30:36 +0200

    changes

---
+++ R/gamma_regression.R
@@ -0,0 +1,10 @@
+#[export]
+gammareg <- function(y, x, tol = 1e-07, maxiters = 100) {
+  gamma_reg(Y = y, X = x, mod = mod, tol = tol, maxiters = maxiters) 
+}
+
+
+#[export]
+gammaregs <- function(y, x, tol = 1e-07, logged = FALSE, parallel = FALSE, maxiters = 100) {
+  gamma_regs(Y = y, X = x, tol = tol, logged = logged, parallel = (parallel > 0), maxiters = maxiters)
+}
