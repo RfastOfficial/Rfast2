4a6b154861e7bb378e7b1b79b6602a6cfe5a770f
Michail Tsagris <mtsagris@yahoo.gr>
2020-01-19 12:18:39 +0200

    updatemle.R

--- R/powerlaw.mle.R
+++
@@ -1,9 +0,0 @@
-#[export]
-powerlaw.mle <- function(x) {
-  n <- length(x)
-  x1 <- min(x)
-  com <- sum( log(x) ) - n * log(x1)
-  a <- 1 + n / com
-  loglik <- n * log( (a - 1) / x1 ) - a * com 
-  list(alpha = a, loglik = loglik)
-}
