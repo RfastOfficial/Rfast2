9e11893166d96b056d37dc759c3510f14fedb62e
Manos Papadakis <papadakm95@gmail.com>
2020-09-22 22:22:11 +0300

    fix bug

--- R/column_wise_mle.R
+++ R/column_wise_mle.R
@@ -86,7 +86,7 @@ colcauchy.mle <- function (x, tol = 1e-07, maxiters = 100, parallel = FALSE) {
 
 
 #[export]
-colbeta.mle <- function(x, tol = 1e-07, maxiters = 100, parallel = FALSE {
+colbeta.mle <- function(x, tol = 1e-07, maxiters = 100, parallel = FALSE) {
     res <- .Call(Rfast2_colbeta_mle, x, tol, parallel, maxiters)
     colnames(res) <- c("alpha", "betag", "loglik")
     res
