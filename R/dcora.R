d074d097c3de48d85f3baaaa51090200db820cfc
statlink <mtsagris@yahoo.gr>
2020-10-08 23:13:22 +0300

    changes

--- R/dcora.R
+++ R/dcora.R
@@ -4,7 +4,7 @@ dcora <- function(x) {
   a <- matrix(0, p, p)
   for ( i in 1:c(p - 1) ) {
     for ( j in c(i + 1):p ) {
-      a[i, j] <- a[j, i] <- Rfast::dcor(x[, i], x[, j])
+      a[i, j] <- a[j, i] <- Rfast::dcor(x[, i], x[, j])$dcor
     }
   }
   diag(a) <- 1
