1c0867a99cb8765c585a15163d11781ff9189a60
statlink <mtsagris@yahoo.gr>
2021-01-11 15:32:31 +0200

    changes

--- R/many_regression_models.R
+++ R/many_regression_models.R
@@ -135,14 +135,11 @@ score.zipregs <- function(y, x, logged = FALSE) {
 
 
 #[export]
-negbin.regs <- function (y, x, tol = 1e-07, maxiters = 100, parallel = FALSE)
-{
+negbin.regs <- function (y, x, tol = 1e-07, maxiters = 100, parallel = FALSE) {
   mod <- .Call(Rfast2_negbin_regs, y, x, tol, maxiters, parallel)
-  names(mod$info) <- c("iters", "BIC", "log-likelihood",
-                       "dispersion")
-  row.names(mod$info) <- names(x)
-
-  names(mod$be) <- c("(intercept)","be")
-  row.names(mod$be) <- names(x)
+  colnames(mod$info) <- c("iters", "BIC", "log-likelihood", "dispersion")
+  row.names(mod$info) <- colnames(x)
+  colnames(mod$be) <- c("(intercept)","be")
+  row.names(mod$be) <- colnames(x)
   list(info = mod$info, be = mod$be)
 }
