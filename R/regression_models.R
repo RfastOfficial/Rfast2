de73067b8f91382a8a8daa75a3e5091ed35f89d6
statlink <mtsagris@yahoo.gr>
2021-02-08 23:44:21 +0200

    changes

--- R/regression_models.R
+++ R/regression_models.R
@@ -196,7 +196,8 @@ hp.reg <- function(y, x, full = FALSE, tol = 1e-07, maxiters = 100) {
   id <- which(y > 0)
   y1[id] <- 1
   prob <- Rfast::glm_logistic(x, y1, full = full, tol = tol, maxiters = maxiters)
-  mod <- Rfast2::ztp.reg(y[id], x[id, ], full = full, tol = tol, maxiters = maxiters)
+  x <- model.matrix(y~., data = as.data.frame(x) )
+  mod <- Rfast2::ztp.reg(y[id], x[id, -1, drop = FALSE], full = full, tol = tol, maxiters = maxiters)
   names(mod$be) <- c( "constant", colnames(x) )
   list(prob = prob, mod = mod)
 }
@@ -650,9 +651,9 @@ zigamma.reg <- function(y, x, full = FALSE, tol = 1e-07, maxiters = 100) {
  y1 <- y
  id <- which(y > 0)
  y1[id] <- 1
- x <- model.matrix(y~., data = as.data.frame(x) )
- prob <- Rfast::glm_logistic(x[, -1], y1, full = full, tol = tol, maxiters = maxiters)
+ prob <- Rfast::glm_logistic(x, y1, full = full, tol = tol, maxiters = maxiters)
  rownames(prob$be) <- colnames(x)
+ x <- model.matrix(y~., data = as.data.frame(x) )
  mod <- Rfast2::gammareg(y[id], x[id, -1, drop = FALSE], tol = tol, maxiters = maxiters)
  colnames(mod$be) <- colnames(x) 
  list(prob = prob, mod = mod)
