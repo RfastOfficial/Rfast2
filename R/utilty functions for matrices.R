ebc2b9b4eccc50fd1dde450ebc17a3d81d5d1b7d
statlink <mtsagris@yahoo.gr>
2021-11-16 22:43:29 +0200

    changes

--- R/utilty functions for matrices.R
+++ R/utilty functions for matrices.R
@@ -1,25 +1,25 @@
 #[export]
 is.lower.tri <- function(x,diag = FALSE) {
-	.Call(Rfast2_is_lower_tri,x,diag)
+	.Call(Rfast2_is_lower_tri, x, diag)
 }
 
 
 
 #[export]
 is.upper.tri <- function(x,diag = FALSE) {
-	.Call(Rfast2_is_upper_tri,x,diag)
+	.Call(Rfast2_is_upper_tri, x, diag)
 }
 
 
 
 #[export]
 is.skew.symmetric<-function(x){
-	.Call(Rfast2_is_skew_symmetric,x)
+	.Call(Rfast2_is_skew_symmetric, x)
 }
 
 
 
 #[export]
 lud <- function(x) {
-	.Call(Rfast2_lud,x)
+	.Call(Rfast2_lud, x)
 }
