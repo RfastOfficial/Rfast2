f8d7284c9cd6d3ae1997cd825ad3e6960c705692
statlink <mtsagris@yahoo.gr>
2022-01-26 09:32:32 +0200

    changes

--- R/means_tests.R
+++ R/means_tests.R
@@ -79,6 +79,35 @@ boot.student2 <- function(x, y, B = 999) {
 
 
 
+#[export]
+stud.ttests <- function(x, y = NULL, ina, logged = FALSE, parallel = FALSE) {
+
+  if ( is.null(y) ) {
+    x1 <- x[ ina == 1, ]
+    x2 <- x[ ina == 2, ]
+    n1 <- sum( ina == 1 )
+    n2 <- length(ina) - n1
+  } else {
+    x1 <- x     ;    n1 <- dim(x1)[1]
+    x2 <- y     ;    n2 <- dim(x2)[1]
+  }
+
+  m1 <- Rfast::colmeans(x1, parallel = parallel)
+  m2 <- Rfast::colmeans(x2, parallel = parallel)
+  s1 <- Rfast::colVars(x1, suma = n1 * m1, parallel = parallel) 
+  s2 <- Rfast::colVars(x2, suma = n2 * m2, parallel = parallel)
+  sp <- ( (n1 - 1) * s1 + (n2 - 1) * s2 ) / (n1 + n2 - 2) 
+  stat <- ( m1 - m2 ) / sqrt(sp * (1/n1 + 1/n2) )
+  dof <- n1 + n2 - 2
+  if ( logged ) {
+    pvalue <- log(2) + pt( abs(stat), dof, lower.tail = FALSE, log.p = TRUE )
+  } else  pvalue <- 2 * pt( abs(stat), dof, lower.tail = FALSE )  
+  result <- cbind(stat, pvalue, dof)
+
+  result
+}
+
+
 
 #[export]
 boot.james <- function(y1, y2, R = 999) {
@@ -167,4 +196,26 @@ boot.hotel2 <- function(y1, y2, R = 999) {
     }
   }
   ( sum(tb > tobs) + 1 )/(B^2 + 1)
+}
+
+
+
+rm.hotel <- function(x, a = 0.05) {
+  ## x is the data set
+  ## a is the level of significance set by default to 0.05
+  m <- Rfast::colmeans(x)
+  s <- cov(x)  ## sample mean vector and covariance matrix
+  n <- dim(x)[1]  ## sample size
+  p <- dim(x)[2]  ##  dimensionality of the data
+  C <-  - diag(p)
+  C[, 1] <- 1
+  A <- C %*% m
+  B <- solve( (C %*% s) %*% C, A)
+  T2 <- n * sum(A * B)
+  test <- (n - p + 1) / (n - 1) / (p - 1) * T2  ## test statistic
+  pvalue <- pf(test, p - 1, n - p + 1, lower.tail = FALSE)  ## p-value of the test statistic
+  crit <- qf(1 - a, p - 1, n - p + 1)  ## critical value of the F disitribution
+  result <- c(test, pvalue, crit, p - 1, n - p + 1)
+  names(result) <- c("test", "p-value", "numer df", "denom df", "critical")
+  list(m = m, result = result)
 }
