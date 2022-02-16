74b6383b0deb602ccb03b3e39e21e93541d8913b
statlink <mtsagris@yahoo.gr>
2022-02-16 15:13:20 +0200

    changes

---
+++ R/col_binary_metrics.R
@@ -0,0 +1,39 @@
+#[export]
+colaccs <- function(group, preds) {
+  Rfast::colmeans( preds == group )
+}
+
+
+#[export]
+colsens <- function(group, preds) {
+  res <- 2 * group - preds
+  tab <- Rfast::colTabulate(res + 2)
+  tab[3, ] / (tab[3, ] + tab[4, ])
+}
+
+
+#[export]
+colspecs <- function(group, preds) {
+  res <- 2 * group - preds
+  tab <- Rfast::colTabulate(res + 2)
+  tab[2, ] / (tab[2, ] + tab[1, ])
+}
+
+
+#[export]
+colprecs <- function(group, preds) {
+  res <- 2 * group - preds
+  tab <- Rfast::colTabulate(res + 2)
+  tab[3, ] / (tab[3, ] + tab[1, ])
+}
+
+
+#[export]
+colfscores <- function(group, preds) {
+  res <- 2 * group - preds
+  tab <- Rfast::colTabulate(res + 2)
+  prec <- tab[3, ] / (tab[3, ] + tab[1, ])
+  rec <- tab[3, ] / (tab[3, ] + tab[4, ])
+  2 * prec * rec / (prec + rec)
+}
+
