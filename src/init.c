6d699551674aa0f55867fe2da9726fa4b3a2acfb
Manos Papadakis <papadakm95@gmail.com>
2022-03-03 15:37:21 +0200

    Update init.c

--- src/init.c
+++ src/init.c
@@ -5,7 +5,7 @@
 
 SEXP Rfast2_benchmark(SEXP exprsSEXP,SEXP envSEXP,SEXP timSEXP,SEXP indicesSEXP);
 SEXP Rfast2_col_group(SEXP x,SEXP y,SEXP length_uniqueSEXP,SEXP methodSEXP);
-SEXP Rfast2_col_Quantile(SEXP xSEXP,SEXP ProbsSEXP,SEXP parallelSEXP);
+SEXP Rfast2_colQuantile(SEXP xSEXP,SEXP ProbsSEXP,SEXP parallelSEXP);
 SEXP Rfast2_is_upper_tri(SEXP xSEXP,SEXP dgSEXP);
 SEXP Rfast2_is_lower_tri(SEXP xSEXP,SEXP dgSEXP);
 SEXP Rfast2_is_skew_symmetric(SEXP xSEXP);
@@ -13,7 +13,7 @@ SEXP Rfast2_lud(SEXP xSEXP);
 SEXP Rfast2_merge(SEXP xSEXP,SEXP ySEXP);
 SEXP Rfast2_mmpc2(SEXP ySEXP,SEXP xSEXP,SEXP max_kSEXP,SEXP thresholdSEXP,SEXP testSEXP,SEXP Ini,SEXP parallelSEXP,SEXP maxitersSEXP,SEXP tolSEXP,SEXP backwardSEXP);
 SEXP Rfast2_Quantile(SEXP xSEXP,SEXP ProbsSEXP);
-SEXP Rfast2_row_Quantile(SEXP xSEXP,SEXP ProbsSEXP,SEXP parallelSEXP);
+SEXP Rfast2_rowQuantile(SEXP xSEXP,SEXP ProbsSEXP,SEXP parallelSEXP);
 SEXP Rfast2_colTrimMean(SEXP xSEXP,SEXP ProbsSEXP,SEXP parallelSEXP);
 SEXP Rfast2_rowTrimMean(SEXP xSEXP,SEXP ProbsSEXP,SEXP parallelSEXP);
 SEXP Rfast2_trimmean(SEXP xSEXP,SEXP aSEXP);
@@ -51,7 +51,7 @@ SEXP Rfast2_gamma_reg(SEXP YSEXP, SEXP XSEXP,SEXP modSEXP,SEXP tolSEXP,SEXP maxi
 static const R_CallMethodDef CallEntries[] = {
   {"Rfast2_benchmark", (DL_FUNC) &Rfast2_benchmark, 4},
   {"Rfast2_col_group", (DL_FUNC) &Rfast2_col_group, 4},
-  {"Rfast2_col_Quantile", (DL_FUNC) &Rfast2_col_Quantile, 3},
+  {"Rfast2_colQuantile", (DL_FUNC) &Rfast2_colQuantile, 3},
   {"Rfast2_is_upper_tri", (DL_FUNC) &Rfast2_is_upper_tri, 2},
   {"Rfast2_is_lower_tri", (DL_FUNC) &Rfast2_is_lower_tri, 2},
   {"Rfast2_is_skew_symmetric", (DL_FUNC) &Rfast2_is_skew_symmetric, 1},
@@ -59,7 +59,7 @@ static const R_CallMethodDef CallEntries[] = {
   {"Rfast2_merge", (DL_FUNC) &Rfast2_merge, 2},
   {"Rfast2_Quantile", (DL_FUNC) &Rfast2_Quantile, 2},
   {"Rfast2_mmpc2", (DL_FUNC) &Rfast2_mmpc2, 10},
-  {"Rfast2_row_Quantile", (DL_FUNC) &Rfast2_row_Quantile, 3},
+  {"Rfast2_rowQuantile", (DL_FUNC) &Rfast2_rowQuantile, 3},
   {"Rfast2_rowTrimMean", (DL_FUNC) &Rfast2_rowTrimMean, 3},
   {"Rfast2_colTrimMean", (DL_FUNC) &Rfast2_colTrimMean, 3},
   {"Rfast2_trimmean", (DL_FUNC) &Rfast2_trimmean, 2},
