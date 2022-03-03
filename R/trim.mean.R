48c253b840625d9551d5c780dd5497d6e265ced4
Manos Papadakis <papadakm95@gmail.com>
2022-03-03 15:37:15 +0200

    Update trim.mean.R

--- R/trim.mean.R
+++ R/trim.mean.R
@@ -3,20 +3,22 @@ trim.mean <- function(x, a = 0.05) {
 	.Call(Rfast2_trimmean, x, a)
 }
 
-
-
 #[export]
 rowTrimMean <- function(x, a = 0.05, parallel = FALSE) {
 	.Call(Rfast2_rowTrimMean, x, a, parallel)
 }
 
-
-
-
 #[export]
 colTrimMean <- function(x, a = 0.05, parallel = FALSE) {
+	UseMethod("colTrimMean")
+}
+
+#[export s3]
+colTrimMean.matrix <- function(x, a = 0.05, parallel = FALSE) {
 	.Call(Rfast2_colTrimMean, x, a, parallel)
 }
 
-
-
+#[export s3]
+colTrimMean.data.frame <- function(x, a = 0.05, parallel = FALSE) {
+	.Call(Rfast2_colTrimMean, x, a, parallel)
+}
