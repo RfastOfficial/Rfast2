7eb08ab0e3665603791273051556cf80a2e354b9
Manos Papadakis <papadakm95@gmail.com>
2023-02-10 18:28:33 +0200

    update

--- R/benchmark.R
+++ R/benchmark.R
@@ -15,7 +15,7 @@ benchmark<-function(...,times,envir=parent.frame(),order=NULL){
         stop("Error: order length must be ",length(exprs))
     res<-.Call(Rfast2_benchmark,exprs,envir,times,order)
     rownames(res)<-nm
-    colnames(res)<-c("min","mean","max")
+    colnames(res)<-c("min","mean","max","CPU","memory")
     class(res)<-"benchmark"
     res
 }
@@ -24,26 +24,38 @@ benchmark<-function(...,times,envir=parent.frame(),order=NULL){
 #[export s3]
 print.benchmark<-function(x,...){
     class(x)<-NULL
-    min_max_res<-x[,c("min","max")]
+    x <- as.data.frame(x)
+    min_mean_max<-c("min","mean","max")
+    min_mean_max_res<-x[,min_mean_max]
     unit<-NULL
     unit_number<-NULL
-    if(any(min_max_res>=1)){
+    if(any(min_mean_max_res>=1)){
         unit<-"seconds"
         unit_number<-1 #nothing
-    }else if(any(min_max_res>=10^-2) && any(min_max_res<1)){
+    }else if(any(min_mean_max_res>=10^-2) && any(min_mean_max_res<1)){
         unit<-"milliseconds"
         unit_number<-1000 # 10^3
-    }else if(any(min_max_res>=10^-3) && any(min_max_res<10^-2)){
+    }else if(any(min_mean_max_res>=10^-3) && any(min_mean_max_res<10^-2)){
         unit<-"milliseconds"
         unit_number<-10000 # 10^4
-    }else if(any(min_max_res>=10^-6) && any(min_max_res<10^-3)){
+    }else if(any(min_mean_max_res>=10^-6) && any(min_mean_max_res<10^-3)){
         unit<-"microseconds"
         unit_number<-1000000 #10^6
     }else{
         stop("Error: times is weird...\n")
     }
+
+    x[,min_mean_max]<-x[,min_mean_max]*unit_number
+    x[,"CPU"] <- paste(round(x[,"CPU"],2),"%",sep = "")
+
+    memories <- c("B"=1,"KB"=1024,"MB"=1024000,"GB"=1024000000)
+    memory <- x[,"memory"] / memories
+    memory_index <- which(memory >= 0 & memory <= 100)[1] #if zero the result will be more than one so pick up the first (Byte)
+    memory <- memory[memory_index]
+    x[,"memory"] <- paste(round(memory,2),names(memory),sep = "")
     
-    x<-x*unit_number
+
+
     cat("  ",unit,"\n")
     print(x)
 }
--- src/Eval.cpp
+++ src/Eval.cpp
@@ -11,32 +11,103 @@ using namespace std;
 using namespace std::chrono;
 using namespace Rcpp;
 
-static NumericVector measure_time(SEXP expr,SEXP env,const int tim){
+#include <windows.h>
+#include <psapi.h>
+
+class PC {
+    ULARGE_INTEGER lastCPU, lastSysCPU, lastUserCPU;
+    int numProcessors;
+    HANDLE self;
+
+public:
+    PC(){
+        SYSTEM_INFO sysInfo;
+        FILETIME ftime, fsys, fuser;
+
+        GetSystemInfo(&sysInfo);
+        numProcessors = sysInfo.dwNumberOfProcessors;
+
+        GetSystemTimeAsFileTime(&ftime);
+        memcpy(&lastCPU, &ftime, sizeof(FILETIME));
+
+        self = GetCurrentProcess();
+        GetProcessTimes(self, &ftime, &ftime, &fsys, &fuser);
+        memcpy(&lastSysCPU, &fsys, sizeof(FILETIME));
+        memcpy(&lastUserCPU, &fuser, sizeof(FILETIME));
+    }
+    double getCurrentCPU(){
+        FILETIME ftime, fsys, fuser;
+        ULARGE_INTEGER now, sys, user;
+        double percent;
+
+        GetSystemTimeAsFileTime(&ftime);
+        memcpy(&now, &ftime, sizeof(FILETIME));
+
+        GetProcessTimes(self, &ftime, &ftime, &fsys, &fuser);
+        memcpy(&sys, &fsys, sizeof(FILETIME));
+        memcpy(&user, &fuser, sizeof(FILETIME));
+        percent = (sys.QuadPart - lastSysCPU.QuadPart) +
+        (user.QuadPart - lastUserCPU.QuadPart);
+        percent /= (now.QuadPart - lastCPU.QuadPart);
+        percent /= numProcessors;
+        lastCPU = now;
+        lastUserCPU = user;
+        lastSysCPU = sys;
+
+        return percent * 100;
+    }
+    SIZE_T getCurrentVirtualMemory(){
+        return getMemoryInfo().PrivateUsage;
+    }
+    SIZE_T getCurrentPhysicalMemory(){
+        return getMemoryInfo().WorkingSetSize;
+    }
+
+private:
+
+    PROCESS_MEMORY_COUNTERS_EX getMemoryInfo(){
+        PROCESS_MEMORY_COUNTERS_EX pmc;
+        GetProcessMemoryInfo(GetCurrentProcess(), (PROCESS_MEMORY_COUNTERS*)&pmc, sizeof(pmc));
+        return pmc;
+    }
+};
+
+SIZE_T distance(SIZE_T a,SIZE_T b){
+    return a > b ? a-b : b-a;
+}
+
+static NumericVector measure_time(SEXP expr,SEXP env,const int tim,PC &pc){
     steady_clock sc;
     NumericVector times(tim);
+    SIZE_T memory = 0;
+    double cpu = 0.0;
     double sum_t=0,max_t,min_t;
     for (int i = 0; i < tim; ++i){
+        SIZE_T mem = pc.getCurrentPhysicalMemory();
         auto start = sc.now();
         Rf_eval(expr,env);
         auto end = sc.now();
+        memory += distance(pc.getCurrentPhysicalMemory(),mem);
+        cpu += pc.getCurrentCPU();
         times[i] = static_cast<chrono::duration<double>>(end - start).count();
         sum_t+=times[i];
     }
     min_max<double>(&times[0],&times[tim-1]+1,min_t,max_t);
-    return NumericVector::create(min_t,sum_t/tim,max_t);
+    return NumericVector::create(min_t,sum_t/tim,max_t,cpu/tim,memory/tim);
 }
 
 //[[Rcpp::export]]
 NumericMatrix benchmark(List exprs,SEXP env,const int tim,IntegerVector indices){
-    NumericMatrix res(exprs.length(),3);
+    NumericMatrix res(exprs.length(),5);
+    PC pc;
     for(auto& index : indices){
-        res.row(index-1) = measure_time(exprs[index-1],env,tim);
+        res.row(index-1) = measure_time(exprs[index-1],env,tim,pc);
     }
     return res;
 }
 
 RcppExport SEXP Rfast2_benchmark(SEXP exprsSEXP,SEXP envSEXP,SEXP timSEXP,SEXP indicesSEXP){
-BEGIN_RCPP
+    BEGIN_RCPP
     RObject __result;
     RNGScope __rngScope;
     traits::input_parameter< List  >::type exprs(exprsSEXP);
@@ -45,7 +116,7 @@ BEGIN_RCPP
     traits::input_parameter< IntegerVector  >::type indices(indicesSEXP);
     __result = benchmark(exprs,env,tim,indices);
     return __result;
-END_RCPP
+    END_RCPP
 }
 
 
@@ -60,28 +131,28 @@ using namespace chrono;
 
 //[[Rcpp::export]]
 string tic_c(){
-    constexpr auto zero_d = time_point<steady_clock>(seconds(0));
-    double s = static_cast<duration<double>>(steady_clock().now()-zero_d).count();
-    return to_string(s);
+constexpr auto zero_d = time_point<steady_clock>(seconds(0));
+double s = static_cast<duration<double>>(steady_clock().now()-zero_d).count();
+return to_string(s);
 }
 
 //[[Rcpp::export]]
 NumericVector duration_s(string x,string y){
-    double d=stod(x)-stod(y);
-    CharacterVector unit="seconds";
-    NumericVector f(1);
-    if(d >= 1.0){ // seconds
-        unit = "seconds";
-    }
-    else if((d*=1e+03) >= 1.0){ //milliseconds
-        unit = "milliseconds";
-    }else{//microseconds
-        unit = "microseconds";
-        d *= 1e+03;
-    }
-    f.names()=unit;
-    f[0]=d;
-    return f;
+double d=stod(x)-stod(y);
+CharacterVector unit="seconds";
+NumericVector f(1);
+if(d >= 1.0){ // seconds
+unit = "seconds";
+}
+else if((d*=1e+03) >= 1.0){ //milliseconds
+unit = "milliseconds";
+}else{//microseconds
+unit = "microseconds";
+d *= 1e+03;
+}
+f.names()=unit;
+f[0]=d;
+return f;
 }
 
 */
