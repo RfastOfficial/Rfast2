665065bb8672566f95164941e0f5c3aff6a27196
Manos Papadakis <papadakm95@gmail.com>
2023-07-07 13:12:56 +0300

    add core argument for parallelization

--- R/Quantile.R
+++ R/Quantile.R
@@ -1,22 +1,22 @@
 #[export]
 Quantile <- function(x, probs) {
-	.Call(Rfast2_Quantile, x, probs)
+  .Call(Rfast2_Quantile, x, probs)
 }
 
 #[export]
-rowQuantile <- function(x, probs, parallel = FALSE) {
-	.Call(Rfast2_rowQuantile, x, probs, parallel)
+rowQuantile <- function(x, probs, parallel = FALSE, cores = 0) {
+  .Call(Rfast2_rowQuantile, x, probs, parallel, cores)
 }
 
 #[export s3]
-colQuantile.data.frame<-function(x, probs, parallel = FALSE) {
-	.Call(Rfast2_colQuantile, x, probs, parallel)
+colQuantile.data.frame <- function(x, probs, parallel = FALSE, cores = 0) {
+  .Call(Rfast2_colQuantile, x, probs, parallel, cores)
 }
 #[export]
-colQuantile<-function(x, probs, parallel = FALSE) {
-	UseMethod("colQuantile")
+colQuantile <- function(x, probs, parallel = FALSE, cores = 0) {
+  UseMethod("colQuantile")
 }
 #[export s3]
-colQuantile.matrix<-function(x, probs, parallel = FALSE) {
-	.Call(Rfast2_colQuantile, x, probs, parallel)
+colQuantile.matrix <- function(x, probs, parallel = FALSE, cores = 0) {
+  .Call(Rfast2_colQuantile, x, probs, parallel, cores)
 }
