1d35e7bdb896adf5563fbf5868e136f368f1ef03
Manos Papadakis <papadakm95@gmail.com>
2023-10-26 20:00:29 +0300

    add manos work

--- R/univariate.mle.R
+++ R/univariate.mle.R
@@ -1,108 +1,22 @@
 #[export]
 censpois.mle <- function(x, tol = 1e-07) {
-  ca <- min(x)
-  z <- 0:ca
-  fac <- factorial(z)
-  n <- length(x)
-  n2 <- sum(x == ca)
-  n1 <- n - n2
-  sx <- sum( x[x > ca] )
-  a1 <- log( sx/n )
-  down <- sum( exp(a1 * z)/fac )
-  dera <-  - n1 * exp(a1) + sx + n2 * sum( z * exp( a1 * z )/fac ) / down
-  dera2 <-  - n1 * exp(a1) + 
-        n2 * ( sum( z^2 * exp( a1 * z/fac ) ) - sum( z * exp(a1 * z)/fac )^2 ) / down^2
-  a2 <- a1 - dera/dera2
-  i <- 2
-  while ( abs(a2 - a1) > tol ) {
-    i <- i + 1
-    down <- sum( exp(a1 * z)/fac )
-    a1 <- a2
-    dera <-  - n * exp(a1) + sx + n2 * sum( z * exp( a1 * z )/fac ) / down
-    dera2 <-  - n * exp(a1) + 
-        n2 * ( sum( z^2 * exp( a1 * z/fac ) ) - sum( z * exp(a1 * z)/fac )^2 ) / down^2
-    a2 <- a1 - dera/dera2  
-  }
-
-  loglik <-  - n * exp(a1) + sx * a1 - sum( lgamma(x + 1) ) + n2 * log( down )
-  list(iters = i, loglik = loglik, lambda = exp(a2) )
+  .Call(Rfast2_censpois_mle,x,tol)
 }
 
+#[export]
+colcenspois.mle <- function(x, tol = 1e-07, parallel = FALSE, cores = 0) {
+  .Call(Rfast2_colcenspois_mle,x,tol, parallel,cores)
+}
 
 #[export]
 censweibull.mle <- function(x, di, tol = 1e-07) {
-  y <- log(x)
-  y1 <- y[di == 1]
-  y2 <- y[di == 0]
-  n <- length(x)
-  n1 <- length(y1)
-  n2 <- n - n1
-  mod <- Rfast::weibull.mle(x[di==1])
-  if ( n2 > 0 ) {
-    m <- log(mod$param[2])
-    es <- 1/mod$param[1]
-    s <- log( es )
-    z1 <- ( y1 - m ) / es
-    z2 <- ( y2 - m ) /es
-    com <- sum(z1)
-    ez1 <- exp(z1) 
-    ez2 <- exp(z2)
-    com1 <- sum(ez1)
-    com2 <- sum(ez2)
-  
-    lik1 <- com - com1 - n1 * s - com2
-
-    derm2 <-  - com1 / es^2 - com2 / es^2  
-    derm <-  - n1/ es + - derm2 * es 
-    ders <-  - com + sum(ez1 * z1) - n1 + sum(ez2 * z2)
-    ders2 <-  com - sum(ez1 * z1^2) - sum(ez1 * z1) - sum(ez2 * z2^2) - sum(ez2 * z2)
-    derms <- n1/es - sum(ez1 * z1) / es - com1/es - sum(ez2 * z2) / es - com2/es 
-    anew <- c(m, s) - c(ders2 * derm - derms * ders, -derms * 
-              derm + derm2 * ders)/(derm2 * ders2 - derms^2)
-    m <- anew[1]
-    s <- anew[2]
-    es <- exp(s)
-    z1 <- ( y1 - m ) / es
-    z2 <- ( y2 - m ) /es
-    com <- sum(z1)
-    ez1 <- exp(z1) 
-    ez2 <- exp(z2)
-    com1 <- sum(ez1)
-    com2 <- sum(ez2)
-  
-    lik2 <- com1 - sum(ez1) - n1 * s - com2
-    i <- 2
-    while ( abs(lik2 - lik1) > tol ) {
-      i <- i + 1
-      lik1 <- lik2
-    
-      derm2 <-  - com1 / es^2 - com2 / es^2  
-      derm <-  - n1/ es + - derm2 * es 
-      ders <-  - com + sum(ez1 * z1) - n1 + sum(ez2 * z2)
-      ders2 <-  com - sum(ez1 * z1^2) - sum(ez1 * z1) - sum(ez2 * z2^2) - sum(ez2 * z2)
-      derms <- n1/es - sum(ez1 * z1) / es - com1/es - sum(ez2 * z2) / es - com2/es 
-      anew <- c(m, s) - c(ders2 * derm - derms * ders, -derms * 
-              derm + derm2 * ders)/(derm2 * ders2 - derms^2)
-      m <- anew[1]
-      s <- anew[2]
-      es <- exp(s)
-      z1 <- ( y1 - m ) / es
-      z2 <- ( y2 - m ) /es
-      com <- sum(z1)
-      ez1 <- exp(z1) 
-      ez2 <- exp(z2)
-      com1 <- sum(ez1)
-      com2 <- sum(ez2)
-    
-      lik2 <- com - com1 - n1 * s - com2
-    }
-    param <- c(exp(m), es)
-    names(param) <- c("scale", "1/shape")
-    mod <- list(iters = i, loglik = lik2 - sum(y1), param = param)
-  } else  mod <- mod
-  mod
+  .Call(Rfast2_censweibull_mle,x, di, tol)
 } 
 
+#[export]
+colcensweibull.mle <- function(x, di, tol = 1e-07, parallel = FALSE, cores = 0) {
+  .Call(Rfast2_colcensweibull_mle,x, di, tol,parallel,cores)
+}
 
 #[export]
 gammapois.mle <- function(x, tol = 1e-07) {
@@ -280,30 +194,12 @@ gammapois.mle <- function(x, tol = 1e-07) {
 
 #[export]
 halfcauchy.mle <- function(x, tol = 1e-07) {
-   n <- length(x)
-   es <- 0.5 * (Rfast::nth(x, 3 * n/4) - Rfast::nth(x, n/4))
-   logs <- log(es)
-   x2 <- x^2
-   down <- 1/(x2 + es^2) 
-   lik1 <- n * logs + sum( log(down) )
-   der <- n - 2 * es^2 * sum(down)
-   der2 <-  - 4 * es^4 * sum(down^2)
-   logs <- logs - der/der2
-   es <- exp(logs)
-   down <- 1/(x2 + es^2)   
-   lik2 <- n * logs + sum( log(down) )
-   i <- 2
-   while ( lik2 - lik1 > tol ) {
-     i <- i + 1
-     lik1 <- lik2
-     der <- n - 2 * es^2 * sum(down)
-     der2 <-  - 4 * es^4 * sum(down^2)
-     logs <- logs - der/der2
-     es <- exp(logs)
-     down <- 1/(x2 + es^2)   
-     lik2 <- n * logs + sum( log(down) )
-   } 
-  list(iters = i, loglik = lik2 - n * log(2/pi), scale = es)
+   .Call(Rfast2_halfcauchy_mle,x,tol)
+}
+
+#[export]
+colhalfcauchy.mle <- function(x, tol = 1e-07, parallel = FALSE, cores = 0) {
+   .Call(Rfast2_colhalfcauchy_mle,x,tol,parallel,cores)
 }
 
 
