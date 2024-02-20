20774ab388d2ac95369323d7decbff0bebc118cd
Manos Papadakis <papadakm95@gmail.com>
2024-02-20 22:05:34 +0200

    Merge branch 'Dev' of https://github.com/RfastOfficial/Rfast2 into Dev

--- R/jbtests.R
+++ R/jbtests.R
@@ -1,10 +1,10 @@
 #[export]
-jbtest <- function(x, logged = FALSE) {
+jbtest <- function(x) {
   n <- length(x)
   S <- Rfast::skew(x)
   K <- Rfast::kurt(x)
   jb <- n / 6 * ( S^2 + 0.25 * (K - 3)^2 )
-  pvalue <- pchisq(jb, 2, lower.tail = FALSE, log.p = logged)
+  pvalue <- pchisq(jb, 2, lower.tail = FALSE)
   res <- c(jb, pvalue)
   names(res) <- c("stat", "p-value")
   res
@@ -12,12 +12,12 @@ jbtest <- function(x, logged = FALSE) {
 
 
 #[export]
-jbtests <- function(x, logged = FALSE) {
+jbtests <- function(x) {
   n <- dim(x)[1]
   S <- Rfast::colskewness(x)
   K <- Rfast::colkurtosis(x)
   jb <- n / 6 * ( S^2 + 0.25 * (K - 3)^2 )
-  pvalue <- pchisq(jb, 2, lower.tail = FALSE, log.p = logged)
+  pvalue <- pchisq(jb, 2, lower.tail = FALSE)
   res <- cbind(jb, pvalue)
   colnames(res) <- c("stat", "p-value")
   res
