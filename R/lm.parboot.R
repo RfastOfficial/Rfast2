3a8769c9b461e0136dc327392840f899224fc21b
Michail Tsagris <mtsagris@yahoo.gr>
2024-11-28 18:46:34 +0200

    change

--- R/lm.parboot.R
+++ R/lm.parboot.R
@@ -1,4 +1,27 @@
 #[export]
+lm.boot <- function(x, y, R = 1000, type = "param") {
+   x <- model.matrix( y~., data.frame(x) )
+   xx <- tcrossprod( Rfast::spdinv( crossprod(x) ), x ) 
+   be <- xx %*% y
+   dm <- dim(x)
+   n <- dm[1]   ;    p <- dm[2]
+   est <- as.vector( x %*% be ) 
+   res <- y - est
+   s <- Rfast::Var(res) * (n - 1) / (n - p)
+    
+   if ( type == "param" ) {
+     z <- Rfast::matrnorm(n, R)
+   } else if ( type == "nonparam" ) {
+     z <- Rfast::rep_col(res, R)
+     z <- Rfast::colShuffle(z)
+   }	 
+   booty <- sqrt(s) * z + est
+   xx %*% booty
+}    
+
+
+
+#[export]
 lm.parboot <- function(x, y, R = 1000) {
    x <- model.matrix( y~., data.frame(x) )
    xx <- tcrossprod( Rfast::spdinv( crossprod(x) ), x ) 
