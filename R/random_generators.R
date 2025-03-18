9748a3259278c698289e17e0f1c06421307c1fe8
Manos Papadakis <papadakm95@gmail.com>
2025-03-18 16:59:14 +0200

    Update random_generators.R

--- R/random_generators.R
+++ R/random_generators.R
@@ -13,38 +13,3 @@ Sample <- function(x, size, replace = FALSE){
 Sample.int <- function(n, size = n, replace = FALSE){
 	.Call(Rfast2_Sample_int, n, size, replace)
 }
-
-#[export]
-Rbeta <- function(n, alpha, beta) {
-    .Call(Rfast2_Rbeta, n, alpha, beta)
-}
-
-#[export]
-Rexp <- function(n, rate = 1.0) {
-    .Call(Rfast2_Rexp, n, rate)
-}
-
-#[export]
-Rchisq <- function(n, df) {
-    .Call(Rfast2_Rchisq, n, df)
-}
-
-#[export]
-Rgamma <- function(n, shape, rate = 1.0) {
-    .Call(Rfast2_Rgamma, n, shape, rate)
-}
-
-#[export]
-Rgeom <- function(n, prob) {
-    .Call(Rfast2_Rgeom, n, prob)
-}
-
-#[export]
-Rcauchy <- function(n, location = 0, scale = 1) {
-    .Call(Rfast2_Rcauchy, n, location, scale)
-}
-
-#[export]
-Rt <- function(n, df, ncp = 0) {
-    .Call(Rfast2_Rt, n, df, ncp)
-}
