23e4f1a2a04c1e810ad171d8789b7dd291ca073b
Manos Papadakis <papadakm95@gmail.com>
2025-03-28 12:13:44 +0200

    Update kernel.cpp

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -3,7 +3,6 @@
 //[[Rcpp::plugins(cpp11)]]
 
 #define ARMA_64BIT_WORD
-
 #include <RcppArmadillo.h>
 
 //[[Rcpp::depends(RcppParallel)]]
@@ -70,10 +69,9 @@ NumericVector kernel(NumericVector X, string h) {
     try {
     if (h == "silverman") {
         NumericVector probs = {0.25,0.75};
-        colvec y = conv_to<colvec>::from(x);
-        colvec tmp = Rfast::Quantile<colvec>(y, probs);
-        // colvec iqr = diff(tmp);
-        // hd = 0.9 * min(s, iqr(0) / 1.34) * std::pow(n, -0.2);
+        colvec tmp = Rfast::Quantile<colvec>(clone(X), probs);
+        colvec iqr = diff(tmp);
+        hd = 0.9 * min(s, iqr(0) / 1.34) * std::pow(n, -0.2);
     } else if (h == "scott") {
         hd = 1.06 * s * std::pow(n, -0.2);
     }else{
