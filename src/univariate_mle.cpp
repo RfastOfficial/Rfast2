76cce25d8a9749162a159fd48ea66faaa50a3c1c
Manos Papadakis <papadakm95@gmail.com>
2025-04-01 13:37:08 +0300

    Update univariate_mle.cpp

--- src/univariate_mle.cpp
+++ src/univariate_mle.cpp
@@ -29,6 +29,7 @@ List halfcauchy_mle(NumericVector x, const double tol = 1e-07)
 	double logs = log(es);
 	vec x2 = square(x1);
 	vec down = 1 / (x2 + es1);
+	down.t().print("down");
 	double lik1 = x1.n_elem * logs + accu(log(down));
 	double der = x1.n_elem - 2 * (es1 * accu(down));
 
